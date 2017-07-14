ffa613e05dc70a5b5d5f6d2cad4a2e10f8b97ce2
Kevin Durbin <kevindurb@gmail.com>
2017-07-14 11:18:49 -0600

    Refactor logic to helpers, add merge and replace

--- test/reducer.js
+++ test/reducer.js
@@ -61,4 +61,48 @@ describe('single entity manipulation', () => {
       assert.strictEqual(store.getState().dog[5], undefined);
     });
   });
+
+  context('#mergeEntity', () => {
+    it('should be able to merge an entity', () => {
+      const store = storeFactory();
+      const fredTheDog = { id: 5, name: 'fred' };
+      const fredTheDogWithAge = { id: 5, name: 'fred', age: 26 };
+
+      store.dispatch(actions.addEntity('dog', fredTheDog));
+      assert.strictEqual(store.getState().dog[5], fredTheDog);
+
+      store.dispatch(actions.mergeEntity('dog', fredTheDogWithAge));
+      assert.deepEqual(store.getState().dog[5], fredTheDogWithAge);
+    });
+
+    it('should create an entity if it doesnt exist', () => {
+      const store = storeFactory();
+      const fredTheDogWithAge = { id: 5, name: 'fred', age: 26 };
+
+      store.dispatch(actions.mergeEntity('dog', fredTheDogWithAge));
+      assert.strictEqual(store.getState().dog[5], fredTheDogWithAge);
+    });
+  });
+
+  context('#replaceEntity', () => {
+    it('should be able to replace an entity', () => {
+      const store = storeFactory();
+      const fredTheDog = { id: 5, name: 'fred' };
+      const fredTheDogWithAge = { id: 5, name: 'fred', age: 26 };
+
+      store.dispatch(actions.addEntity('dog', fredTheDog));
+      assert.strictEqual(store.getState().dog[5], fredTheDog);
+
+      store.dispatch(actions.replaceEntity('dog', fredTheDogWithAge));
+      assert.strictEqual(store.getState().dog[5], fredTheDogWithAge);
+    });
+
+    it('should create an entity if it doesnt exist', () => {
+      const store = storeFactory();
+      const fredTheDogWithAge = { id: 5, name: 'fred', age: 26 };
+
+      store.dispatch(actions.replaceEntity('dog', fredTheDogWithAge));
+      assert.strictEqual(store.getState().dog[5], fredTheDogWithAge);
+    });
+  });
 });
